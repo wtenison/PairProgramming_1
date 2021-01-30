272cbab35ff83684c685f6e5aa524b56ec4a40b5
wtenison <wtenison@protonmail.com>
2021-01-29 16:28:46 -0900

    Intial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int foo(std::string inputString)
+{
+    if (inputString.size() == 0)
+        return 0;
+
+    return 1;
+}
+
+int main()
+{
+    return 1;
+}
