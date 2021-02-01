df65a9231d62046cf4b08876dc1f7e790fe88cf9
Hunter <whbarndt@protonmail.com>
2021-01-31 20:34:46 -0900

    Replaced tests.cpp code with Factorial Test Code

--- tests.cpp
+++ tests.cpp
@@ -1,15 +1,13 @@
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 
-int calculate(std::string inputString)
-{
-    if (!inputString.size())
-        return 0;
-
-    return 1;
+unsigned int Factorial( unsigned int number ) {
+    return number <= 1 ? number : Factorial(number-1)*number;
 }
 
-TEST_CASE("Caclulate tests")
-{
-    REQUIRE( calculate("") == 0 );
+TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(10) == 3628800 );
 }
