f67acfb55b5d7a901570bd522175724840763ec5
wtenison <wtenison@protonmail.com>
2021-02-04 14:32:44 -0900

    Pass 1 (Wyatt)

--- tests.cpp
+++ tests.cpp
@@ -1,11 +1,18 @@
 #define CATCH_CONFIG_MAIN
+#include <vector>
 #include "catch.hpp"
 
 int calculate(std::string inputString)
 {
+    std::vector<std::string> tokens;
+
+    // Check for empty string
     if (!inputString.size())
         return 0;
 
+    // Break up the input string into variables
+    while(inputString)    
+    
     return 1;
 }
 
