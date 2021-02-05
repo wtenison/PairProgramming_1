9a61d3a84178b55c8c2be4d63a964f11ca4072b6
Hunter <whbarndt@protonmail.com>
2021-02-04 15:01:09 -0900

    Pass 2 (Hunter)

--- tests.cpp
+++ tests.cpp
@@ -1,22 +1,36 @@
 #define CATCH_CONFIG_MAIN
 #include <vector>
+#include <iostream>
 #include "catch.hpp"
 
 int calculate(std::string inputString)
 {
     std::vector<std::string> tokens;
+    std::stringstream string(inputString);
+    int sum = 0;
 
     // Check for empty string
     if (!inputString.size())
         return 0;
 
     // Break up the input string into variables
-    while(inputString)    
+    while(inputString != "")
+    {
+        std::string saved;
+        getline(string, saved, ",");
+        tokens.append(saved);
+    }    
     
-    return 1;
+    for(auto i : tokens)
+    {
+        sum += atoi(i)
+    }
+
+    return sum;
 }
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
+TEST_CASE( "Factorials are computed", "[factorial]" ) 
+{
     REQUIRE( Factorial(1) == 1 );
     REQUIRE( Factorial(2) == 2 );
     REQUIRE( Factorial(3) == 6 );
