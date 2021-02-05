6bffd499173a0576a95762324c38fb2e1a235260
wtenison <wtenison@protonmail.com>
2021-02-04 16:52:01 -0900

    Final pass

--- tests.cpp
+++ tests.cpp
@@ -8,7 +8,6 @@
 int calculate(std::string inputString)
 {
     std::vector<std::string> tokens;
-    std::stringstream s_string(inputString);
 
     int sum = 0;
 
@@ -16,17 +15,30 @@ int calculate(std::string inputString)
     if (!inputString.size())
         return 0;
 
-    // Break up the input string into variables
+    printf("NOTE: on %s\n", inputString.c_str());
+
+    size_t indexOf_newline = inputString.find('\n');
+    while (indexOf_newline != std::string::npos && indexOf_newline != inputString.size())
+    {
+        inputString.at(indexOf_newline) = ',';
+        printf("NOTE: Replaced at %d\n", indexOf_newline);
+        indexOf_newline = inputString.find('\n');
+    }
+
+    std::stringstream s_string(inputString);
+    printf("OUT: %s\n", inputString.c_str());
+    // Break up the input string into tokens
     while(s_string.good())
     {
         std::string saved("");
         std::getline(s_string, saved, ',');
         tokens.push_back(saved);
-    }    
+    }
     
     // Add up the positive numbers
     for(std::string i : tokens)
     {
+        printf("ADDING: %d to %d\n", atoi(i.c_str()), sum);
         if (atoi(i.c_str()) > -1)
             sum += atoi(i.c_str());
         else
